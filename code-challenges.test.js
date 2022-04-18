4a76270e7b4ff67eacfd609bc67a723b52fb4790
lundrew <andrueee@gmail.com>
2022-04-17 19:55:38 -0700

    Completed Challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,21 +15,31 @@
 
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+// const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+// // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+// const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+// // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
 
-// b) Create the function that makes the test pass.
+describe('remFirstShuff', () => {
+    const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+    const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+    it('takes in an array, removes the first item from the array and shuffles the remaining content', () => {
+      expect(remFirstShuff(colors1)).toEqual(expect.arrayContaining(colors1));
+      expect(remFirstShuff(colors2)).toEqual(expect.arrayContaining(colors2));
+    });
+  });
 
 
-// create a function called "remFirstShuff" 
-// input param of "array"
-// use shift() method to remove first element of array
-// create a for loop to iterate through the array
-// include Math.random() method to randomize the array
-// return the new randomized array
+// // b) Create the function that makes the test pass.
+
+
+// // create a function called "remFirstShuff" 
+// // input param of "array"
+// // use shift() method to remove first element of array
+// // create a for loop to iterate through the array
+// // include Math.random() method to randomize the array
+// // return the new randomized array
 
 const remFirstShuff = (array) => {
 
@@ -47,74 +57,83 @@ const remFirstShuff = (array) => {
 
 
 
+// // --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
+
+// // a) Create a test with expect statements for each of the variables provided.
+
+// const nums1 = [3, 56, 90, -8, 0, 23, 6]
+// // Expected output: [-8, 90]
+// const nums2 = [109, 5, 9, 67, 8, 24]
+// // Expected output: [5, 109]
+
+describe('minMax', () => {
+    const nums1 = [3, 56, 90, -8, 0, 23, 6]
+    const nums2 = [109, 5, 9, 67, 8, 24]
+    it('takes an array of numbers and returns an array of the minimum and maximum numbers in that order', () => {
+        expect(minMax(nums1)).toEqual([-8, 90]);
+        expect(minMax(nums2)).toEqual([5, 109]);
+    });
+  });
 
 
+// // b) Create the function that makes the test pass.
 
-
-
-
-
-
-
-// --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
-
-// a) Create a test with expect statements for each of the variables provided.
-
-const nums1 = [3, 56, 90, -8, 0, 23, 6]
-// Expected output: [-8, 90]
-const nums2 = [109, 5, 9, 67, 8, 24]
-// Expected output: [5, 109]
-
-
-// b) Create the function that makes the test pass.
-
-// create a function called "minMax"
-// create param called "array"
-// create a variable called "max" and "min"
-// use reduce() method to obtain only 1 value
-// create a function and use the Math.max/Math.min methods to obtain the max number and min number
-// return the min and max number
+// // create a function called "minMax"
+// // create param called "array"
+// // create a variable called "max" and "min"
+// // use reduce() method to obtain only 1 value
+// // create a function and use the Math.max/Math.min methods to obtain the max number and min number
+// // return the min and max number
 
 const minMax = (array) => {
     
     var max = array.reduce(function(a, b) {
         return Math.max(a, b) 
     })
+
     var min = array.reduce(function(a, b) {
         return Math.min(a, b) 
     })
+
     return [min, max] 
 
 }
+
+
  
 
 
+// // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
+
+// // a) Create a test with an expect statement using the variables provided.
+
+// const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+// const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+// // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
 
 
+describe('combine', () => {
+    const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+    const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+    it('takes in two arrays as arguments and returns one array with no duplicate values', () => {
+      expect(combine(testArray1, testArray2)).toEqual([3, 7, 10, 5, 4, 8, 2, 1]);
+    });
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
-
-// a) Create a test with an expect statement using the variables provided.
-
-const testArray1 = [3, 7, 10, 5, 4, 3, 3]
-const testArray2 = [7, 8, 2, 3, 1, 5, 4]
-// Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
 
 
 // b) Create the function that makes the test pass.
+
+const combine = (array1, array2) => {
+
+    let combined = array1.concat(array2)
+    let uniqueChars = [];
+
+combined.forEach((element) => {
+    if (!uniqueChars.includes(element)) {
+        uniqueChars.push(element);
+    }
+
+});
+    return uniqueChars
+}
--- interview-questions.md
+++ interview-questions.md
@@ -7,52 +7,60 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What are props in React?
 
-  Your answer:
+  Your answer: Props are properties in React and can be passed from component to component in React. 
 
-  Researched answer:
+  Researched answer: Props are arguments passed into React components. The reason we use props is to pass data from one component to another (from parent component to a child component). Props is a shorter way of saying properties. Props are passed to the component in a similar way as arguments are passed in a function. 
 
 
 
 2. What is a DOM event?
 
-  Your answer:
+  Your answer: A DOM event is when an action takes place on the webpage. This could be like an onClick event for example. 
 
-  Researched answer:
+  Researched answer: DOM events are signals that something has occurred or can be triggered by user interactions or by the browser. 
+  Examples of DOM events include:
+  - When a user clicks the mouse
+  - When a web page has loaded
+  - When an image has been loaded
+  - When the mouse moves over an element
+  - When an input field is changed
+  - When an HTML form is submitted
+  - When a user presses a key
 
 
 
 3. What is object-oriented programming? How is it different than functional programming?
 
-  Your answer:
+  Your answer: Object-oriented programming is a philosophy that focuses on creating through objects within javascript. 
 
-  Researched answer:
+  Researched answer: Object-oriented programming or OOP prevents data from moving freely around the system. OOP ties data securely within a function that operates on it. The reason for OOP is to protect the data from accidental modification from an outside function. 
 
 
 
 4. What is the difference between a Float and an Integer in Ruby?
 
-  Your answer:
+  Your answer: An integer is a number and a float is a number with a decimal. 
 
-  Researched answer:
+  Researched answer: Floats and integers are two different kinds of numerical data. An integer is a number without a decimal point where a float is a floating-point number which means it has a decimal place. Floats are used when precision is needed. 
 
 
 
 5. Ruby has an implicit return. What does that mean?
 
-  Your answer:
+  Your answer: an implicit return is where we can return some code without the keyword "return". Implicit return is an implied return. 
 
-  Researched answer:
+  Researched answer: Ruby's implicit return will return the value of the last line automatically without including the "return" key word. 
 
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Instance Variable:
+1. Instance Variable: In Ruby, an instance variable is a type of variable that starts with an "@" symbol. An instance variable is used as part of Object-Oriented Programming to give objects their own private space to staore data. Instance variables store data and know things. 
 
-2. PostgreSQL:
+2. PostgreSQL: PostgreSQL is a stable database management system used to store data for many web, mobile, geospatial and analytics applications. 
 
-3. Ruby on Rails:
+3. Ruby on Rails: Ruby on Rails or Rails is a server side web application framework. Rails provides default structures for a database, a web service and web pages. 
 
-4. ORM:
+4. ORM: ORM or Object-Relational Mapping is a technique for converting data between type systems using object-oriented programming languages. 
 
-5. Active Record:
+5. Active Record: Active Record is an ORM and is a layer of Ruby code that runs between your database and your logic code. 
